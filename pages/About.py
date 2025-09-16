8f473a5cc3f8d4829b09edcee06c3166179e63e0
Bamsesbuddy <amanda@jacobsson.st>
2025-09-16 14:38:42 +0200

    Create about page
    Started text about my last project

---
+++ pages/About.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.markdown("# Amanda Jacobsson")
+
+""" During the last course of data science I completed a machine model for predicting if a movie would be rated as a top 100 movies.  
+My data set contained information about the movies name, rating, ranking, length ofg movie in minutes and decade of release or publication.  
+My model was not really that good in predicting and my final proposal was that the model could instead be switched to finding out ....  """
