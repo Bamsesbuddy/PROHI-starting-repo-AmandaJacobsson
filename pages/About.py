5b28f228daa7cd1cc2e0af07adda6c6fe858b819
Bamsesbuddy <amanda@jacobsson.st>
2025-09-18 13:43:51 +0200

    Final version for submission

--- pages/About.py
+++ pages/About.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.markdown("# Amanda Jacobsson")
 
-""" During the last course of data science I completed a machine model for predicting if a movie would be rated as a top 100 movies.  
-My data set contained information about the movies name, rating, ranking, length of movie in minutes and year of release or publication. Excluded from the file wa genre and and gross value.   
-My model was not really that good in predicting and my final proposal was that the model could instead predict which movies would be highly ranked, and therefore a good suggestion, based on year, rating and number of votes.  """
+""" During the last course of data science, I completed a machine model for predicting if a movie would be rated as a top 100 movies.
+My data set contained information about the movie’s name, rating, ranking, length of movie in minutes and year of release or publication. Excluded from the file was genre and gross value.
+My model was not really that good in predicting and my final proposal was that the model could instead predict which movies would be highly ranked, and therefore a good suggestion, based on year, rating and number of votes.
+ """
